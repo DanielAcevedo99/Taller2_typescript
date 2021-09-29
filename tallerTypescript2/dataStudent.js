1790a402173a067511da6b8e2973299f1eac1eae
Daniel Acevedo <df.acevedo@uniandes.edu.co>
2021-09-29 16:56:33 -0500

    taller 2

---
+++ tallerTypescript2/dataStudent.js
@@ -0,0 +1,4 @@
+import { Student } from './student.js';
+export var dataStudent = [
+    new Student("201910941", "1020844988", 21, "Carrera 54D #189-59", "3188098174"),
+];
---
+++ tallerTypescript2/dataStudent.ts
@@ -0,0 +1,5 @@
+import { Student } from './student.js';
+
+export const dataStudent = [
+  new Student("201910941", "1020844988", 21, "Carrera 54D #189-59", "3188098174"),
+] 
